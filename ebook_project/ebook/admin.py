430b0c71db10c56b1380f437dacb407854cb62ae
FatemaAlFailakawi <fatema.alfailakawi@cba.ku.edu.kw>
2022-01-06 23:04:53 +0300

    Review and order models

--- ebook_project/ebook/admin.py
+++ ebook_project/ebook/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
-from .models import Book,Genra
-
+from .models import Book,Genre
+from .models import Customer, Order, Review
 # Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+  pass
+
+class OrderAdmin(admin.ModelAdmin):
+  pass
+
+class ReviewAdmin(admin.ModelAdmin):
+  pass
 
 class BookAdmin(admin.ModelAdmin):
   pass
-class GenraAdmin(admin.ModelAdmin):
+class GenreAdmin(admin.ModelAdmin):
   pass
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Order,OrderAdmin)
+admin.site.register(Review, ReviewAdmin)
 admin.site.register(Book, BookAdmin)
-admin.site.register(Genra,GenraAdmin)
+admin.site.register(Genre,GenreAdmin)
--- ebook_project/ebook/models.py
+++ ebook_project/ebook/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.db.models.fields import TextField
 
 # Create your models here.
 
 class Customer(models.Model):
+    customer_id = models.IntegerField(max_length=12, null=False, unique=True)
     CName = models.CharField(max_length=50, null=False)
     date_of_birth = models.DateField(null=False)
     email = models.EmailField(blank=False, null=False)
@@ -11,9 +13,16 @@ class Customer(models.Model):
     credit_card = models.CharField(max_length=16, null=True)
 
 
+class Order(models.Model):
+    order_id = models.CharField(max_length=12, null=False, unique=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
+class Review(models.Model):
+    review_id = models.CharField(max_length=10, null=False, unique=True)
+    comment = TextField()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
-
+    
 class Book(models.Model):
     title=models.CharField(max_length=100, null=False)
     discription= models.CharField(max_length=150, null=False)
@@ -23,7 +32,7 @@ class Book(models.Model):
 def __str__(self):
     return self.title
 
-class Genra(models.Model):
+class Genre(models.Model):
     type=models.CharField(max_length=50, null=False)
     allbook = models.ForeignKey(
         'Book',
