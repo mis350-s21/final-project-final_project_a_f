1d85a10a2192d31472c813d586ce85f400d8b7c9
FatemaAlFailakawi <fatema.alfailakawi@cba.ku.edu.kw>
2022-01-06 19:27:36 +0300

    Customer model

--- ebook_project/ebook/models.py
+++ ebook_project/ebook/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    CName = models.CharField(max_length=50, null=False)
+    date_of_birth = models.DateField(null=False)
+    email = models.EmailField(blank=False, null=False)
+    phone = models.CharField(max_length=8, null=False, unique=True)
+    address = models.CharField(max_length=100, null=False)
+    credit_card = models.CharField(max_length=16, null=True)
