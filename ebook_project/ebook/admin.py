12d7130b1971937b42eb6f6b8cd9c10ed3458314
Aisha Aldurais <93175552+Aysha9s@users.noreply.github.com>
2022-01-06 21:43:32 +0300

    commit-ticket3 model only

--- ebook_project/ebook/admin.py
+++ ebook_project/ebook/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book,Genra
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+  pass
+class GenraAdmin(admin.ModelAdmin):
+  pass
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genra,GenraAdmin)
--- ebook_project/ebook/models.py
+++ ebook_project/ebook/models.py
@@ -9,3 +9,26 @@ class Customer(models.Model):
     phone = models.CharField(max_length=8, null=False, unique=True)
     address = models.CharField(max_length=100, null=False)
     credit_card = models.CharField(max_length=16, null=True)
+
+
+
+
+
+class Book(models.Model):
+    title=models.CharField(max_length=100, null=False)
+    discription= models.CharField(max_length=150, null=False)
+    price= models.FloatField(max_length=10, null=False)
+    inventory= models.CharField(max_length=50, null=False)
+      
+def __str__(self):
+    return self.title
+
+class Genra(models.Model):
+    type=models.CharField(max_length=50, null=False)
+    allbook = models.ForeignKey(
+        'Book',
+        on_delete=models.CASCADE,
+    )
+
+def __str__(self):
+    return self.type 
