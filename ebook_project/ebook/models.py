d250b8ea8471ecc6911bb2244e2c10983e25d624
Aisha Aldurais <93175552+Aysha9s@users.noreply.github.com>
2022-01-07 12:05:33 +0300

    second commit

--- ebook_project/ebook/models.py
+++ ebook_project/ebook/models.py
@@ -4,13 +4,15 @@ from django.db.models.fields import TextField
 # Create your models here.
 
 class Customer(models.Model):
-    customer_id = models.IntegerField(max_length=12, null=False, unique=True)
+    #customer_id = models.IntegerField(null=False, unique=True)
     CName = models.CharField(max_length=50, null=False)
     date_of_birth = models.DateField(null=False)
     email = models.EmailField(blank=False, null=False)
     phone = models.CharField(max_length=8, null=False, unique=True)
     address = models.CharField(max_length=100, null=False)
     credit_card = models.CharField(max_length=16, null=True)
+    def __str__(self):
+        return self.CName
 
 
 class Order(models.Model):
@@ -18,8 +20,8 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 class Review(models.Model):
-    review_id = models.CharField(max_length=10, null=False, unique=True)
-    comment = TextField()
+    review_id = models.CharField(max_length=12,null=False, unique=True)
+    comment = models.TextField()
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
     
@@ -29,15 +31,13 @@ class Book(models.Model):
     price= models.FloatField(max_length=10, null=False)
     inventory= models.CharField(max_length=50, null=False)
       
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 class Genre(models.Model):
     type=models.CharField(max_length=50, null=False)
-    allbook = models.ForeignKey(
-        'Book',
-        on_delete=models.CASCADE,
-    )
+    allbook=models.ManyToManyField(Book)
 
-def __str__(self):
-    return self.type 
+    #allbook = models.ForeignKey('Book',on_delete=models.CASCADE, )
+    def __str__(self):
+        return self.type 
