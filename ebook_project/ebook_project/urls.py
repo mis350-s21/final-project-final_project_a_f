14ab9a997ec3a98c1ce6d9cc21b616fb36e4c857
FatemaAlFailakawi <fatema.alfailakawi@cba.ku.edu.kw>
2022-01-17 23:59:00 +0300

    create book

--- ebook_project/ebook_project/urls.py
+++ ebook_project/ebook_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from ebook.views import welcom
+from ebook.views import welcome
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ebook/', include('ebook.urls')),
-    path('',welcom),
+    path('',welcome)
 ]
